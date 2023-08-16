a07cfbcbcd1f9b7be2fb092f54cfd9ac82231166
okyryliuk <o.kyryliuk.i@gmail.com>
2023-08-16 12:43:16 +0300

    config swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+
+  const swaggerConfig = new DocumentBuilder()
+  .setTitle('Game swagger: ')
+  // .setDescription('The cats API description')
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+const document = SwaggerModule.createDocument(app, swaggerConfig);
+SwaggerModule.setup('swagger', app, document);
+
+await app.listen(process.env.PORT);
+console.log(`Server is running on port  ${process.env.PORT}`);
+
 }
 bootstrap();
