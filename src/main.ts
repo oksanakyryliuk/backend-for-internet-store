a5a6fbe3694a10866d611f44d4e024752da44653
okyryliuk <o.kyryliuk.i@gmail.com>
2023-08-20 20:55:00 +0300

    add validationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   const swaggerConfig = new DocumentBuilder()
-  .setTitle('Book-Discovery swagger: ')
-  // .setDescription('The cats API description')
-  .setVersion('1.0')
-  .addBearerAuth()
-  .build();
-const document = SwaggerModule.createDocument(app, swaggerConfig);
-SwaggerModule.setup('swagger', app, document);
+    .setTitle('Book-Discovery swagger: ')
+    // .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('swagger', app, document);
 
-await app.listen(process.env.PORT);
-console.log(`Server is running on port  ${process.env.PORT}`);
+  await app.listen(process.env.PORT);
+  console.log(`Server is running on port  ${process.env.PORT}`);
 
 }
 bootstrap();
