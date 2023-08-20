68c5d8e7a47ea13d064f297a35284c9ad96406ee
okyryliuk <o.kyryliuk.i@gmail.com>
2023-08-20 23:18:00 +0300

    add enableCors, limit on json and add globalPrefix

--- src/main.ts
+++ src/main.ts
@@ -6,26 +6,28 @@ import { ValidationPipe, Logger } from '@nestjs/common'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.use('json', { limit: '10mb' });
+  app.setGlobalPrefix('api');
+  app.enableCors();
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Book-Discovery swagger: ')
-    // .setDescription('The cats API description')
+    .setDescription('Internet book store')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('swagger', app, document);
 
-  await app.listen(process.env.PORT, ()=>{
-try {
-  Logger.verbose('Server is listening at port: ' + process.env.PORT);
-  Logger.verbose(`Open server in web: localhost:${process.env.PORT}/`);
-  Logger.warn(`Open SWAGGER in web: localhost:${process.env.PORT}/swagger`);
-}
-catch (e){
-  Logger.error(e);
-}})
-
-
+  await app.listen(process.env.PORT, () => {
+    try {
+      Logger.verbose('Server is listening at port: ' + process.env.PORT);
+      Logger.verbose(`Open server in web: localhost:${process.env.PORT}/`);
+      Logger.warn(`Open SWAGGER in web: localhost:${process.env.PORT}/swagger`);
+    }
+    catch (e) {
+      Logger.error(e);
+    }
+  })
 }
 bootstrap();
