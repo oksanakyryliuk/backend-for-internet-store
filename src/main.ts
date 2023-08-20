60851d22b5b8f88d8ee668a7525c0302ae89737f
okyryliuk <o.kyryliuk.i@gmail.com>
2023-08-20 22:53:44 +0300

    add logger to app

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
-import { ValidationPipe } from '@nestjs/common'
+import { ValidationPipe, Logger } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,8 +16,16 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('swagger', app, document);
 
-  await app.listen(process.env.PORT);
-  console.log(`Server is running on port  ${process.env.PORT}`);
+  await app.listen(process.env.PORT, ()=>{
+try {
+  Logger.verbose('Server is listening at port: ' + process.env.PORT);
+  Logger.verbose(`Open server in web: localhost:${process.env.PORT}/`);
+  Logger.warn(`Open SWAGGER in web: localhost:${process.env.PORT}/swagger`);
+}
+catch (e){
+  Logger.error(e);
+}})
+
 
 }
 bootstrap();
